8000b5d2995807dc31bffafd1ae02d439dadf86c
EvgeniyaDubrovskaya <zenya@tut.by>
2018-10-07 22:30:52 +0300

    Add test cases for checking sotring of countries and geo zones

--- LiteCardTests/LiteCardTests/CountriesGeozonesSotring.cs
+++ LiteCardTests/LiteCardTests/CountriesGeozonesSotring.cs
@@ -54,8 +54,10 @@ namespace LiteCardTests
 
 
         [Test]
-        public void GeoZoneSortingTest()
+        public void GeoZoneSortingOnCountryPageTest()
         {
+            bool isZonesSorted = false;
+            
             //Login as admin
             driver.Url = "http://localhost/litecart/admin/";
             driver.FindElement(By.Name("username")).SendKeys("admin");
@@ -82,17 +84,65 @@ namespace LiteCardTests
             {
                 driver.Url = url;
                 wait.Until(driver => driver.Title.ToString().Contains("My Store"));
-                var wbGeoZones = driver.FindElements(By.CssSelector("#table-zone td input[name*='name']"));
+                var wbGeoZones = driver.FindElements(By.CssSelector(".dataTable input[name*='name'][type='hidden']"));
 
                 List<string> zones = new List<string>();
                 foreach (var wbGeoZone in wbGeoZones)
                 {
                     zones.Add(wbGeoZone.GetAttribute("value"));
                 }
-                Assert.True(isListSorted(zones));
+                isZonesSorted = isListSorted(zones);
             }
-            
+
+            Assert.True(isZonesSorted);
+
         }
+
+        [Test]
+        public void GeoZoneSortingTest()
+        {
+            bool isZonesSorted = false;
+
+            //Login as admin
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(driver => driver.Title.ToString().Equals("My Store"));
+
+            //Go to Zones page
+            driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+            wait.Until(driver => driver.Title.ToString().Equals("Geo Zones | My Store"));
+
+            //Get all countries
+            var wbRows = driver.FindElements(By.CssSelector(".dataTable td a:not([title=Edit]"));
+            List<string> urls = new List<string>();
+
+            //Get countries uls wich has Geozone
+            foreach (var wbRow in wbRows)
+            {
+                urls.Add(wbRow.GetAttribute("href"));
+            }
+
+            foreach (var url in urls)
+            {
+                driver.Url = url;
+                wait.Until(driver => driver.Title.ToString().Contains("My Store"));
+                var wbGeoZones = driver.FindElements(By.CssSelector(".dataTable select[name*='zone_code'] option[selected='selected']"));
+
+                List<string> zones = new List<string>();
+                foreach (var wbGeoZone in wbGeoZones)
+                {
+                    zones.Add(wbGeoZone.GetAttribute("textContent"));
+                }
+                isZonesSorted = isListSorted(zones);
+            }
+
+            Assert.True(isZonesSorted);
+
+        }
+
+
 
 
         [TearDown]
